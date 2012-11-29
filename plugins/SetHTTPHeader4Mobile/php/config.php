6c2c4874764e2f03885cf5c8043d7310bcc0affe
Alfasado Inc <webmaster@alfasado.jp>
2012-11-29 20:44:11 +0900

    Shift-JIS to SJIS.

--- plugins/SetHTTPHeader4Mobile/php/config.php
+++ plugins/SetHTTPHeader4Mobile/php/config.php
@@ -10,7 +10,7 @@ class SetHTTPHeader4Mobile extends MTPlugin {
 
     function build_page ( $mt, $ctx, &$args, &$content ) {
         $app = $ctx->stash( 'bootstrapper' );
-        $sjis = mb_check_encoding( $content, 'Shift-JIS' );
+        $sjis = mb_check_encoding( $content, 'SJIS' );
         if ( $sjis ) {
             $app->send_http_header( 'text/html; charset=shift_jis', filemtime( $file ), strlen( $content ) );
             echo $content;
