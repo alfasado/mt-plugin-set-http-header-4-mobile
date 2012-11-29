8d75c0a6dfdac7519272400ee3562e4f4e2b0d1e
Alfasado Inc <webmaster@alfasado.jp>
2012-11-29 17:19:31 +0900

    first commit

---
+++ plugins/SetHTTPHeader4Mobile/php/config.php
@@ -0,0 +1,22 @@
+<?php
+class SetHTTPHeader4Mobile extends MTPlugin {
+
+    var $registry = array(
+        'name' => 'SetHTTPHeader4Mobile',
+        'callbacks' => array(
+            'build_page' => 'build_page'
+        ),
+    );
+
+    function build_page ( $mt, $ctx, &$args, &$content ) {
+        $app = $ctx->stash( 'bootstrapper' );
+        $sjis = mb_check_encoding( $content, 'Shift-JIS' );
+        if ( $sjis ) {
+            $app->send_http_header( 'text/html; charset=shift_jis', filemtime( $file ), strlen( $content ) );
+            echo $content;
+            exit();
+        }
+    }
+}
+
+?>
